6ce8fca9820d8fe7634cace9a49c7b6c7d981ac0
YanYamim <yanyamim@gmail.com>
2025-01-10 12:49:09 -0300

    Implementação do método PUT para edição de lanches

--- Lab 3/Lanches/Lanches/src/main/java/com/example/Backend/Controller/LancheController.java
+++ Lab 3/Lanches/Lanches/src/main/java/com/example/Backend/Controller/LancheController.java
@@ -5,24 +5,24 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Backend.Entity.Lanche;
-import com.example.Backend.Interfaces.LancheRepository;
 import com.example.Backend.Services.LancheService;
 
 @RestController
 @RequestMapping("/lanches")
 @CrossOrigin(origins = "*")
 public class LancheController {
-    private LancheRepository lancheRepository;
     private LancheService lancheService;
 
-    public LancheController(LancheRepository lancheRepository, LancheService lancheService) {
-        this.lancheRepository = lancheRepository;
+    public LancheController(LancheService lancheService) {
         this.lancheService = lancheService;
     }
 
@@ -31,6 +31,12 @@ public class LancheController {
 
         Lanche lancheSalvo = lancheService.cadastrarLanche(dados.getLancheNome(), dados.getLancheTipo());
         return ResponseEntity.ok(lancheSalvo);
+    }
+
+    @PutMapping("editar/{id}")
+    public ResponseEntity<Lanche> atualizarLanche(@PathVariable("id") Long lancheId, @RequestBody Lanche lanche) {
+        Lanche lancheAtualizado = lancheService.atualizarLanche(lancheId, lanche.getLancheNome(), lanche.getLancheTipo());
+        return ResponseEntity.ok(lancheAtualizado);
     }
 
     @GetMapping("listar")
--- Lab 3/Lanches/Lanches/src/main/java/com/example/Backend/Services/LancheService.java
+++ Lab 3/Lanches/Lanches/src/main/java/com/example/Backend/Services/LancheService.java
@@ -1,12 +1,16 @@
 package com.example.Backend.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.Backend.Entity.Lanche;
 import com.example.Backend.Interfaces.LancheRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class LancheService {
@@ -23,6 +27,20 @@ public class LancheService {
         novoLanche.setLancheTipo(lancheTipo);
         Lanche lancheSalvo = lancheRepository.save(novoLanche);
         return lancheSalvo;
+    }
+
+    public Lanche atualizarLanche(@PathVariable Long lancheId, String lancheNome, String lancheTipo) {
+        Optional<Lanche> lancheExistente = lancheRepository.findById(lancheId);
+
+        if(lancheExistente.isPresent()) {
+            Lanche lancheParaEditar = lancheExistente.get();
+            lancheParaEditar.setLancheNome(lancheNome);
+            lancheParaEditar.setLancheTipo(lancheTipo);
+
+            return lancheRepository.save(lancheParaEditar);
+        }
+
+        throw new EntityNotFoundException("Lanche não encontrado pelo ID" + lancheId);
     }
 
     public List<Lanche> listarLanche() {
