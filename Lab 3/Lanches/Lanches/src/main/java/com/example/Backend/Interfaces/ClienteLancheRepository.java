bb571f1d1e2a3967643173578391e48f246d9f94
YanYamim <yanyamim@gmail.com>
2025-01-12 13:35:18 -0300

    Começando Service que relaciona Cliente e Lanche

---
+++ Lab 3/Lanches/Lanches/src/main/java/com/example/Backend/Interfaces/ClienteLancheRepository.java
@@ -0,0 +1,9 @@
+package com.example.Backend.Interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteLancheRepository  {
+    
+}
